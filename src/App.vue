e2a60eba3e1348d4f63b84e1694535bb445a8588
阿木 <sennes111762@gmail.com>
2024-05-17 18:24:02 +0800

    🔧 refactor: Update App.vue layout and style for Lucky Hash Game
    * Update the App.vue file to include a new layout and style for the Lucky Hash Game
    * Replace the old logo and HelloWorld component with a new navbar and input field for the game
    * Modify the tailwind.config.js file to include two new DaisyUI themes, "dim" and "cupcake"
    * Remove unnecessary CSS rules from the style.css file
    
    This commit refactors the App.vue file to better suit the needs of the Lucky Hash Game by updating the layout and style. The old logo and HelloWorld component have been replaced with a new navbar and input field for the game. Additionally, the tailwind.config.js file has been updated to include two new DaisyUI themes, "dim" and "cupcake", which can be used to customize the appearance of the game. Finally, unnecessary CSS rules have been removed from the style.css file to keep the code clean and organized.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <!-- container -->
+  <div class="container mx-auto px-4">
+    <!-- navbar -->
+    <div class="navbar rounded-btn bg-base-300">
+      <div class="navbar-start">
+        <a class="btn btn-ghost normal-case text-xl">
+          logo here
+        </a>
+      </div>
+      <div class="navbar-center">
+        <a class="btn btn-ghost text-xl">Lucky Hash Game</a>
+      </div>
+      <div class="navbar-end">
+        <div class="badge badge-primary badge-outline">V0.0.1</div>
+      </div>
+    </div>
+    <!-- content -->
+    <div class="center-flex">
+      <input type="text" pattern="\d{5}" maxlength="5" placeholder="00000" oninput="value=value.replace(/[^\d]/g,'')" class="input" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <div class="container mx-auto px-4">
+
+  </div>
 </template>
 
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.center-flex {
+  display: flex; /* 启用Flexbox布局 */
+  justify-content: center; /* 水平居中 */
+  margin: 20px auto;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.center-flex .input {
+  width: 260px; 
+  text-align: center; /* 使文本居中 */
+  font-size: 36px; /* 字号加大 */
+  font-weight: bold; /* 加粗 */
+  letter-spacing: 25px; /* 增加字符间距 */
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
